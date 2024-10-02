65c34dec9a9f635ff0c78806fa34cadb38471566
KDG <rlaehrbs9702@naver.com>
2024-10-02 22:25:09 +0900

    Lv 2 - 7 : Fetch Join으로 N + 1 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId") // Lv 2 - 7 : Fetch Join으로 N + 1 해결
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
